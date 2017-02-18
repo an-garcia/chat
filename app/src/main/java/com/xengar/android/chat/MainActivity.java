9a537d38b6742508616664e23f3358ab7783f5ea
Angel Newton <xanewton@gmail.com>
2017-02-18 16:14:22 -0600

    Exit app when press back button

--- app/src/main/java/com/xengar/android/chat/MainActivity.java
+++ app/src/main/java/com/xengar/android/chat/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.xengar.android.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -216,6 +217,21 @@ public class MainActivity extends AppCompatActivity {
         }
         mMessageAdapter.clear();
         detachDatabaseReadListener();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
 
